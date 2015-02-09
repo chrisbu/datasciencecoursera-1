2a7271cab480213ab8c4f4f41ecc407e5f87d6a0
Rener Castro <tuskan.net@gmail.com>
2015-02-08 22:33:15 -0200

    R Classes, programming Assignments 1

---
+++ corr.R
@@ -0,0 +1,18 @@
+source("complete.R");
+
+corr <- function(directory, threshold = 0, pollution=NULL) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  
+  if(is.null(pollution)) pollution <- load(directory);
+  complete.obs = complete("specdata",data=pollution);
+  pollution.threshold = subset(pollution, pollution[,"ID"] %in% complete.obs[complete.obs[,"nobs"] > threshold,"id"]);
+  as.numeric(by(pollution.threshold, factor(pollution.threshold[,"ID"]) , function (pollution) cor(pollution[,"nitrate"],pollution[,"sulfate"],use="complete.obs") ));
+}
